6ef08ec0945a2c7c1a7c0426d489de0d47f4b3aa
Fath Imtiaz <fathimtiaz@gmail.com>
2024-02-27 17:32:50 +0700

    update profile repo

--- repository/implementations.go
+++ repository/implementations.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/SawitProRecruitment/UserService/domain"
 )
@@ -35,6 +36,40 @@ func (r *Repository) GetUserById(ctx context.Context, id int64) (user domain.Use
 		SELECT id, full_name, phone, password FROM user_
 		WHERE id = $1
 		`, id).Scan(&user.Id, &user.Name, &user.Phone, &user.Password)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (r *Repository) UpdateUser(ctx context.Context, input UpdateUserInput) (user domain.User, err error) {
+	var setQ string = "SET "
+	var whereQ string = "WHERE phone = $"
+	var argCount = 0
+	var args []interface{}
+
+	if input.Phone != "" {
+		argCount++
+		setQ += fmt.Sprintf("phone = $%d", argCount)
+		args = append(args, input.Phone)
+	}
+
+	if input.FullName != "" {
+		argCount++
+		setQ += fmt.Sprintf("full_name = $%d", argCount)
+		args = append(args, input.FullName)
+	}
+
+	setQ += "\n"
+
+	argCount++
+	whereQ += fmt.Sprintf("%d", argCount)
+	args = append(args, input.WhereId)
+
+	err = r.Db.QueryRowContext(ctx, `
+		UPDATE user_
+		`+setQ+whereQ, args...).Scan()
 	if err != nil {
 		return
 	}
--- repository/types.go
+++ repository/types.go
@@ -1,2 +1,8 @@
 // This file contains types that are used in the repository layer.
 package repository
+
+type UpdateUserInput struct {
+	WhereId  string
+	Phone    string
+	FullName string
+}
